4a9f743ac244bac725b965567b9a4ba8f4b38bc4
SAHIL VERMA <132760049+sahilverma1512@users.noreply.github.com>
2025-03-11 18:31:49 +0530

    Update middleware.js
    Middle ware arcjet detectr bot is commment out

--- middleware.js
+++ middleware.js
@@ -9,32 +9,51 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 
-const aj = arcjet({
-  key : process.env.ARCJET_KEY,
-  rules:[
-    shield({
-      mode : 'LIVE'
-    }),
-    detectBot({
-      mode : "LIVE",
-      allow:[
-        "CATEGORY:SEARCH_ENGINE" , "GO_HTTP"
-      ],
-    }),
-  ]
-})
-const clerk =  clerkMiddleware(async(auth,req)=>{
+// const aj = arcjet({
+//   key : process.env.ARCJET_KEY,
+//   rules:[
+//     shield({
+//       mode : 'LIVE'
+//     }),
+//     detectBot({
+//       mode : "LIVE",
+//       allow:[
+//         "CATEGORY:SEARCH_ENGINE" , "GO_HTTP"
+//       ],
+//     }),
+//   ]
+// })
+// const clerk =  clerkMiddleware(async(auth,req)=>{
+//     const {userId} = await auth()
+
+//     if(!userId && isProtectedRoute(req)){
+//         const {redirectToSignIn} = await auth();
+//         return redirectToSignIn();
+//     }
+
+// });
+// export default createMiddleware(aj,clerk);
+
+export default clerkMiddleware(async(auth,req)=>{
     const {userId} = await auth()
 
     if(!userId && isProtectedRoute(req)){
         const {redirectToSignIn} = await auth();
         return redirectToSignIn();
     }
-
 });
 
 
-export default createMiddleware(aj,clerk);
+
+
+
+
+
+
+
+
+
+
 
 export const config = {
   matcher: [
